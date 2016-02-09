c50866f62f57eab2e4efef34abb3c419497e4bf0
Holly Tancredi <tancredi.holly@gmail.com>
2016-02-09 18:24:07 -0500

    Add identicon struct implementation.

--- identicon.go
+++ identicon.go
@@ -4,12 +4,36 @@ import (
     "crypto/md5"
     "flag"
     "fmt"
+    "image/color"
 )
 
-// Hashed string
+// Set up command line flag variables
 var hashString string
 func init() {
     flag.StringVar(&hashString, "hs", "Hello", "String to hash")
+}
+
+// Create the identicon image
+type identicon struct {
+    size        int
+    rows        int
+    cols        int
+    hashedBytes []byte
+}
+
+func New(hashedBytes []byte) *identicon {
+    return &identicon {
+        size: 70,
+        rows: 5,
+        cols: 5,
+        hashedBytes: hashedBytes,
+    }
+}
+
+func (id *identicon) createIdenticon() {
+    background := color.RGBA{255, 255, 255, 255}
+    r, g, b := id.hashedBytes[0:2]
+    pixelColor := color.RGBA{r, g, b, 1}
 }
 
 func main() {
@@ -19,9 +43,9 @@ func main() {
 
     hashed := md5.New()
     hashed.Write([]byte(hashString))
-    byteString := hashed.Sum(nil)
+    hashedBytes := hashed.Sum(nil)
 
     fmt.Println("The string to hash: ", hashString)
-    fmt.Printf("Hash in hex: %x\n", byteString)
-    fmt.Println("Hash in bytes: ", byteString)
+    fmt.Printf("Hash in hex: %x\n", hashedBytes)
+    fmt.Println("Hash in bytes: ", hashedBytes)
 }
